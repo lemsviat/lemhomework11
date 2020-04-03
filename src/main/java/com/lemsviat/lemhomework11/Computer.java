25354608ca42c09ca10ae0000cd07cf5013e3a3f
Sviatoslav Lementar <lemsviat@gmail.com>
2020-04-03 11:01:07 +0300

    First commit

---
+++ src/main/java/com/lemsviat/lemhomework11/Computer.java
@@ -0,0 +1,14 @@
+package main.java.com.lemsviat.lemhomework11;
+
+import java.util.Random;
+
+public class Computer implements Playable{
+
+    @Override
+    public States getStates() {
+        States[] states = States.values();
+        Random random = new Random();
+        int index = random.nextInt(states.length);
+        return states[index];
+    }
+}
---
+++ src/main/java/com/lemsviat/lemhomework11/Playable.java
@@ -0,0 +1,5 @@
+package main.java.com.lemsviat.lemhomework11;
+
+public interface Playable {
+    public States getStates();
+}
